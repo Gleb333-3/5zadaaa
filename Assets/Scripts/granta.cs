00e49fa2fc0e1417db5ae3962a1e11907c66823d
Gleb333-3 <glebefremenko23@gmail.com>
2021-03-19 22:11:06 +0300

    commit

---
+++ Assets/Scripts/granta.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _Granata;
+
+    [SerializeField]
+    private GameObject _Granataobject;
+
+    private void Start()
+    {
+        _Granataobject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _Granataobject.SetActive(true);
+
+        }
+        if (_Granataobject.activeSelf)
+        {
+            _Granata.position = Vector3.MoveTowards(_Granata.position, transform.position, 3 * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/gra.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gra : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Sozdaiomobject;
+
+    [SerializeField]
+    private Transform _Granatmesto;
+
+    [SerializeField]
+    private GameObject _Gran;
+
+    [SerializeField]
+    private Transform _GranataTransform;
+
+    private void Start()
+    {
+        _Gran.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _Gran.SetActive(true);
+
+        }
+         if (_Gran.activeSelf)
+        {
+         _Granatmesto.position = Vector3.MoveTowards(_Granatmesto.position, transform.position, 13 * Time.deltaTime);
+  }
+        int a = 1; 
+        var dis = Vector3.Distance(_GranataTransform.position, _Granatmesto.position);
+        if(dis < a)
+        {
+            Destroy(_Gran);
+            Instantiate(_Sozdaiomobject, _Granatmesto.position, Quaternion.identity);
+            }
+        }
+    }
